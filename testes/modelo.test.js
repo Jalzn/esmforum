d11123375f7086cc08fc68aca0d8aa29ff5c77e9
Jalzn <jalmirfsjr@gmail.com>
2024-12-09 18:55:08 -0300

    Cria novos testes de integracao para modelo

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -16,10 +16,50 @@ test('Testando cadastro de três perguntas', () => {
   modelo.cadastrar_pergunta('1 + 1 = ?');
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
-  const perguntas = modelo.listar_perguntas(); 
+  const perguntas = modelo.listar_perguntas();
   expect(perguntas.length).toBe(3);
   expect(perguntas[0].texto).toBe('1 + 1 = ?');
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
   expect(perguntas[2].num_respostas).toBe(0);
-  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta - 1);
 });
+
+test('Testando get_pergunta com pergunta inexistente', () => {
+  const pergunta = modelo.get_pergunta(9999); // ID inexistente
+  expect(pergunta).toBeUndefined();
+});
+
+test('Testando respostas para pergunta sem respostas', () => {
+  const idPergunta = modelo.cadastrar_pergunta('1+1 = ?');
+
+  const respostas = modelo.get_respostas(idPergunta);
+
+  expect(respostas.length).toBe(0);
+});
+
+test('Testando cadastro de tres respostas', () => {
+  const idPergunta = modelo.cadastrar_pergunta('1+1 = ?')
+
+  modelo.cadastrar_resposta(idPergunta, "Sim!")
+  modelo.cadastrar_resposta(idPergunta, "Sim!")
+  modelo.cadastrar_resposta(idPergunta, "Nao?")
+
+  const respostas = modelo.get_respostas(idPergunta)
+
+  expect(respostas.length).toBe(3)
+  expect(respostas[0].texto).toBe("Sim!")
+  expect(respostas[1].texto).toBe("Sim!")
+  expect(respostas[2].texto).toBe("Nao?")
+})
+
+test('Testando total de respostas para uma pergunta', () => {
+  const idPergunta = modelo.cadastrar_pergunta('1+1 = ?')
+
+  modelo.cadastrar_resposta(idPergunta, "Sim!")
+  modelo.cadastrar_resposta(idPergunta, "Sim!")
+  modelo.cadastrar_resposta(idPergunta, "Nao?")
+
+  const total = modelo.get_num_respostas(idPergunta)
+
+  expect(total).toBe(3)
+})
